a6848ac8c27a951de1eaee57a2dced5a42294f53
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-01-18 15:27:24 +0100

    Implement full pyton bindings for traversability component

--- src/traversability_pybind.cpp
+++ src/traversability_pybind.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include "../perception-traversability/src/traversability.hpp"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -19,6 +20,18 @@ PYBIND11_MODULE(traversability_pybind, m) {
            add
            subtract
     )pbdoc";
+
+    py::class_<traversability::Traversability>(m, "Traversability")
+        .def(py::init<>())
+        .def("configure_traversability", &traversability::Traversability::configureTraversability)
+        .def("set_elevation_map", &traversability::Traversability::setElevationMap)
+        .def("elevation_map_interpolate", &traversability::Traversability::elevationMapInterpolate)
+        .def("elevation_map_2_slope_map", &traversability::Traversability::elevationMap2SlopeMap)
+        .def("threshold_slope_map", &traversability::Traversability::thresholdSlopeMap)
+        .def("dilate_traversability", &traversability::Traversability::dilateTraversability)
+        .def("compute_traversability", &traversability::Traversability::computeTraversability)
+        .def("local_2_global_orientation", &traversability::Traversability::local2globalOrientation)
+        .def("local_2_global_orientation_legacy", &traversability::Traversability::local2globalOrientation_legacy);
 
     m.def("add", &add, R"pbdoc(
         Add two numbers
